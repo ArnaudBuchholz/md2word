fc57795ed8a9f8c37fa37176731441a0c5047898
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-25 15:55:35 -0400

    Lint and render by default

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 'use strict'
 
+require('colors')
 const marked = require('marked')
 const { readFile } = require('fs').promises
+const linter = require('./linter')
+const renderer = require('./renderer')
 
-const renderer = {
-  _check (next) {
-    if (this._last === 'L' && next !== 'LC') {
-      throw new Error('Missing caption for listing : ' + this._lastContent)
-    }
-  },
-
-  _output (command, content) {
-    this._check(command)
-    this._last = command
-    this._lastContent = content
-    return `${command} ${content}\n`
-  },
-
-  heading (text, level) {
-    this._output(`H${level}`, text)
-  },
-
-  paragraph (text) {
-    this._output('P', text)
-  },
-
-  code (code, info, escaped) { 
-    this._output('L', code
-    .replace(/%/g, '%%')
-    .replace(/\r?\n/g, '%N')
-    )
-  },
-
-  blockquote (text) {
-    if (this._last === 'L') {
-      this._output('LC', text)
-    }
-    this._output('Q', text)
-  }
-}
-
-marked.use({ renderer })
-
-const input = process.argv[2]
-console.log(input)
-readFile(input)
-  .then(md => md.toString())
-  .then(md => {
-    console.log(marked.lexer(md))
+readFile(process.argv[2])
+  .then(buffer => buffer.toString())
+  .then(markdown => {
+    const tokens = marked.lexer(markdown)
+    linter(tokens)
+    renderer(tokens, function (command, ...parameters) {
+      if (command.startsWith('end-')) {
+        --this.indent
+      }
+      let formattedCommand = command.magenta
+      if (command.endsWith('-caption')) {
+        formattedCommand = formattedCommand.underline
+      }
+      console.log(' '.repeat(2 * this.indent), formattedCommand, ...parameters.map(parameter => {
+        if (typeof parameter === 'string') {
+          return parameter.gray
+        }
+        if (typeof parameter === 'object') {
+          return parameter
+        }
+        return parameter.toString().yellow
+      }))
+      if (command.startsWith('begin-')) {
+        ++this.indent
+      }
+    }.bind({ indent: 0 }))
   })
