4a748c2be674cdc58592e014cd184e1f00336bba
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-29 00:19:25 -0400

    wip

--- tests/simulator.js
+++ tests/simulator.js
@@ -1,22 +1,41 @@
 'use strict'
 
+function format (tagName) {
+  const from = 0
+  this.texts.every(text => {
+    const end = text.length
+    if (end < this.selectFrom) {
+      return true
+    }
+    if (this.pos)
+
+
+
+  })
+}
+
 const actions = {
   text: function (text) {
+    delete selectFrom
     this.texts.push(text)
     this.pos += text.length
   },
 
   left: function (count) {
+    delete this.selectFrom
     this.pos -= parseInt(count, 10)
   },
 
   select: function (count) {
-    this.start = this.pos
+    this.selectFrom = this.pos
     this.pos += parseInt(count, 10)
   }
 }
 
 function html () {
+  if (this.selectFrom !== undefined) {
+    format.call(this, 'selected')
+  }
   const text = this.texts[0]
   if (this.pos < text.length) {
     return text.substring(0, this.pos)
@@ -28,6 +47,7 @@ function html () {
 
 module.exports = commands => {
   const context = {
+    formats : [],
     texts: [],
     pos: 0
   }
