c51e1a6b0ed725ed018d99085e827b82a6206cfc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-05 09:34:00 -0400

    custom rule

--- linter/non-formatted-header.js
+++ linter/non-formatted-header.js
@@ -1,20 +1,25 @@
+'use strict'
+
 module.exports = {
-  'names': [ 'non-formatted-header' ],
-  'description': 'Formatted headers are forbidden',
-  'tags': [ 'word' ],
-  'function': function rule(params, onError) {
-    console.log(params.tokens)
-/*
-    params.tokens.filter(function filterToken(token) {
-      return token.type === 'blockquote_open';
-    }).forEach(function forToken(blockquote) {
-      var lines = blockquote.map[1] - blockquote.map[0];
-      onError({
-        'lineNumber': blockquote.lineNumber,
-        'detail': 'Blockquote spans ' + lines + ' line(s).',
-        'context': blockquote.line.substr(0, 7)
-      });
-    });
-*/
+  names: ['non-formatted-header'],
+  description: 'Formatted headers are forbidden',
+  tags: ['word'],
+  function: (params, onError) => {
+    let inHeading = false
+    params.tokens.forEach(token => {
+      if (token.type === 'heading_open') {
+        inHeading = true
+      } else if (token.type === 'heading_close') {
+        inHeading = false
+      } else if (inHeading) {
+        if (token.children.length !== 1) {
+          onError({
+            lineNumber: token.lineNumber,
+            detail: 'Formatted headers are forbidden',
+            context: token.line
+          })
+        }
+      }
+    })
   }
 }
