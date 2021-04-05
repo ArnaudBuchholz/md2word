ff42ac029c3d54c67d0b274405beaedecff959f8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-05 10:48:47 -0400

    Dump tokens if debug

--- tests/markdownlint.js
+++ tests/markdownlint.js
@@ -5,12 +5,23 @@ const { readdir } = require('fs').promises
 const samples = join(__dirname, 'linter')
 const rules = join(__dirname, '../linter')
 
+const dump = {
+  names: ['dump-tokens'],
+  description: 'dump-tokens',
+  tags: ['word'],
+  function: (params, onError) => {
+    if (params.tokens.some(token => token.line === 'debug')) {
+      params.tokens.forEach(token => console.log(token))
+    }
+  }
+}
+
 Promise.all([
   readdir(samples),
   readdir(rules)
 ])
   .then(([sampleFiles, ruleFiles]) => markdownlint({
     files: sampleFiles.map(name => join(samples, name)),
-    customRules: ruleFiles.map(name => require(join(rules, name)))
+    customRules: [dump].concat(ruleFiles.map(name => require(join(rules, name))))
   }))
   .then(console.log)
