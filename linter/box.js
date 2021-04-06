8828e572b0ffb042821642adc48bb5399b6bd180
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-06 08:09:36 -0400

    Boxes validation

---
+++ linter/box.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const POSSIBLE_TITLE = 1
+const BOX_BODY = 2
+const END_OF_BOX = 3
+
+module.exports = {
+  names: ['box'],
+  description: 'Check box description',
+  tags: ['word'],
+  function: (params, onError) => {
+    let state = 0
+    let possibleBox
+    let formattedTitle
+    params.tokens
+      .forEach(token => {
+        if (token.type === 'blockquote_open') {
+          if (!state) {
+            possibleBox = token
+            state = POSSIBLE_TITLE
+            formattedTitle = false
+          } else {
+            if (formattedTitle) {
+              onError({
+                lineNumber: possibleBox.lineNumber,
+                detail: 'Box title must be a non-formatted one liner',
+                context: possibleBox.line
+              })
+            }
+            state = BOX_BODY
+          }
+        }
+        if (token.type === 'inline') {
+          if (state === POSSIBLE_TITLE) {
+            formattedTitle = token.children.length !== 1
+          } else if (state === END_OF_BOX) {
+            onError({
+              lineNumber: token.lineNumber,
+              detail: 'Box title after box content',
+              context: possibleBox.line
+            })
+          }
+        }
+        if (token.type === 'blockquote_close') {
+          if (state === BOX_BODY) {
+            state = END_OF_BOX
+          } else if (state === END_OF_BOX) {
+            state = 0
+          }
+        }
+      })
+  }
+}
