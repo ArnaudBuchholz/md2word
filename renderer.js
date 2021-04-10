12578bf014e697f89532b0ac426be0a4aac27b51
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-10 01:01:28 -0400

    wip

--- renderer.js
+++ renderer.js
@@ -2,11 +2,15 @@
 
 const nop = token => console.log(token) // () => {}
 
-/*
+const softbreak = Symbol('softbreak')
+
 const escape = text => text
   .replace(/%/g, '%%')
   .replace(/\n?\n/g, '%N')
-*/
+
+function _reset () {
+  this.text = []
+}
 
 const renderers = {
   inline (token) {
@@ -15,7 +19,7 @@ const renderers = {
 
   heading_open (token) {
     this.format = token.tag
-    this.text = []
+    _reset.call(this)
   },
 
   heading_close (token) {
@@ -24,11 +28,59 @@ const renderers = {
     this.output(`left ${text.length}`)
     this.output(`select ${text.length}`)
     this.output(`format ${this.format}`)
+    this.output(`enter`)
   },
 
   text (token) {
     this.text.push(token.content)
-  }
+  },
+
+  blockquote_open () {
+    this._inBlockQuote = true
+  },
+
+  paragraph_open () {
+    _reset.call(this)
+  },
+
+  softbreak () {
+    this.text.push(softbreak)
+  },
+
+  paragraph_close () {
+    if (this.text.length === 1 && this.text[0] === softbreak || this._inBlockQuote) {
+      return // ignore
+    }
+    const text = this.text.map(t => t === softbreak ? '%N' : t).join('')
+    this.output(`text ${text}`)
+    // Process formatting (if any)
+    this.output(`enter`)
+  },
+
+  blockquote_close () {
+    if (this._nextIsCaption) {
+      const text = this.text.map(t => t === softbreak ? '%N' : t).join('')
+      this.output(`text ${text}`)
+      this.output(`left ${text.length}`)
+      this.output(`select ${text.length}`)
+      this.output('format caption')
+      this.output(`enter`)
+      delete this._nextIsCaption
+    }
+  },
+
+  fence (token) {
+    _reset.call(this)
+    const text = escape(token.content)
+    this.output(`text ${text}`)
+    this.output(`left ${text.length}`)
+    this.output(`select ${text.length}`)
+    this.output(`format code ${token.info}`)
+    this.output(`enter`)
+    this._nextIsCaption = true
+  },
+
+
 }
 
 // 
