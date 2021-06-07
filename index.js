3a85b48e0061d1e8cb2f91061cf6f924fcfe1a8b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-06-06 23:30:43 -0400

    Expose generated script

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const md = require('markdown-it')()
 const { readFile } = require('fs').promises
 const renderer = require('./renderer')
+const { check, log, serve } = require('reserve')
+
+const instructions = []
 
 readFile(process.argv[2])
   .then(buffer => buffer.toString())
@@ -10,5 +13,23 @@ readFile(process.argv[2])
     return md.parse(markdown)
   })
   .then(tokens => {
-    renderer(tokens, console.log)
+    renderer(tokens, instruction => instructions.push(instruction))
+    return check({
+      port: 53475,
+      mappings: [{
+        match: '\\/script$',
+        custom: (request, response) => {
+          const script = instructions.join('\n')
+          const length = (new TextEncoder().encode(script)).length
+          response.writeHead(200, {
+            'content-type': 'text/plain; charset=utf-8',
+            'content-length': length
+          })
+          response.end(script)
+        }
+      }]
+    })
+  })
+  .then(configuration => {
+    log(serve(configuration))
   })
