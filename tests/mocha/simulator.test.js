b5e04b4c43c7ba0592846b0d0c112c0805372c90
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-06-12 01:06:47 -0400

    adds more tests

--- tests/mocha/simulator.test.js
+++ tests/mocha/simulator.test.js
@@ -3,60 +3,67 @@
 const assert = require('assert')
 const simulator = require('../simulator')
 
-const test = (source, expected) => {
+const test = (index, source, expected) => {
   let label = source.replace(/\n/g, ',')
   if (label.length > 70) {
     label = label.substring(0, 67) + '...'
   }
-  it(label, () => {
+  it(`[${index.toString().padStart(2, '0')}] ${label}`, () => {
     const html = simulator(source)
     assert.strictEqual(html, expected)
   })
 }
 
 describe('simulator', () => {
-  test('text Hello World', 'Hello World<cursor/>')
+  test(0, 'text Hello World', 'Hello World<cursor/>')
 
-  test(`text Hello World
+  test(1, `text Hello World
 left 11`, '<cursor/>Hello World')
 
-  test(`text Hello World
+  test(2, `text Hello World
 left 5`, 'Hello <cursor/>World')
 
-  test(`text Hello 
+  test(3, `text Hello 
 text World
 left 5`, 'Hello <cursor/>World')
 
-  test(`text Hello 
+  test(4, `text Hello 
 text Wor
 text ld
 left 5`, 'Hello <cursor/>World')
 
-  test(`text Hello World
+  test(5, `text Hello World
 left 5
 select 5`, 'Hello <selected>World</selected><cursor/>')
 
-  test(`text Hello World
+  test(6, `text Hello World
 left 5
 select 5
 format bold`, 'Hello <b><selected>World</selected></b><cursor/>')
 
-  test(`text Hello World
+  test(7, `text Hello World
 left 5
 select 5
 format bold
 right 1`, 'Hello <b>World</b><cursor/>')
 
-  test(`text Hello World
+  test(8, `text Hello World
+left 5
+select 5
+format bold
+left 1`, 'Hello <cursor/><b>World</b>')
+
+  test(9, `text Hello World
 left 11
 select 11
 format bold
+right 1
 left 5
 select 5
 format italic
 right 1`, '<b>Hello <i>World</i></b><cursor/>')
 
-  test(`text Chapter 1
+  test(10, `text Chapter 1
 left 9
 select 9
 format header1
@@ -65,14 +72,47 @@ text Hello World
 left 5
 select 5
 format bold
-left 3
+left 1
+right 2
 select 2
 format italic
+right 1`, '<h1>Chapter 1</h1>Hello <b>Wo<i>rl</i><cursor/>d</b>')
+
+  test(11, `text Chapter 1
+left 9
+select 9
+format header1
+enter
+text Hello World
+left 5
+select 5
+format bold
+left 1
+right 2
+select 2
+format italic
+right 1
+select 1`, '<h1>Chapter 1</h1>Hello <b>Wo<i>rl</i><selected>d</selected></b><cursor/>')
+
+  test(12, `text Chapter 1
+left 9
+select 9
+format header1
+enter
+text Hello World
+left 5
+select 5
+format bold
+left 1
+right 2
+select 2
+format italic
+right 1
 select 1
 format underline
 right 1`, '<h1>Chapter 1</h1>Hello <b>Wo<i>rl</i><u>d</u></b><cursor/>')
 
-  test(`text Chapter 1
+  test(13, `text Chapter 1
 left 9
 select 9
 format header1
@@ -81,9 +121,11 @@ text Hello World
 left 5
 select 5
 format bold
-left 3
+left 1
+right 2
 select 2
 format italic
+right 1
 select 1
 format underline
 enter`, '<h1>Chapter 1</h1>Hello <b>Wo<i>rl</i><u>d</u></b><br><cursor/>')
