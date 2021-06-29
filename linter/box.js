811e490f365831c0a04acf26c25d49d30c561bd9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-06-29 07:22:16 -0400

    Renaming & reformatting

--- linter/box.js
+++ linter/box.js
@@ -5,49 +5,48 @@ const BOX_BODY = 2
 const END_OF_BOX = 3
 
 module.exports = {
-  names: ['box'],
-  description: 'Check box description',
-  tags: ['word'],
+  names: ['md2word/box'],
+  description: 'Blockquote to box validation',
+  tags: ['md2word'],
   function: (params, onError) => {
     let state = 0
     let possibleBox
     let formattedTitle
-    params.tokens
-      .forEach(token => {
-        if (token.type === 'blockquote_open') {
-          if (!state) {
-            possibleBox = token
-            state = POSSIBLE_TITLE
-            formattedTitle = false
-          } else {
-            if (formattedTitle) {
-              onError({
-                lineNumber: possibleBox.lineNumber,
-                detail: 'Box title must be a non-formatted one liner',
-                context: possibleBox.line
-              })
-            }
-            state = BOX_BODY
-          }
-        }
-        if (token.type === 'inline') {
-          if (state === POSSIBLE_TITLE) {
-            formattedTitle = token.children.length !== 1
-          } else if (state === END_OF_BOX) {
+    params.tokens.forEach(token => {
+      if (token.type === 'blockquote_open') {
+        if (!state) {
+          possibleBox = token
+          state = POSSIBLE_TITLE
+          formattedTitle = false
+        } else {
+          if (formattedTitle) {
             onError({
-              lineNumber: token.lineNumber,
-              detail: 'Box title after box content',
+              lineNumber: possibleBox.lineNumber,
+              detail: 'Box title must be a non-formatted one liner',
               context: possibleBox.line
             })
           }
+          state = BOX_BODY
         }
-        if (token.type === 'blockquote_close') {
-          if (state === BOX_BODY) {
-            state = END_OF_BOX
-          } else {
-            state = 0
-          }
+      }
+      if (token.type === 'inline') {
+        if (state === POSSIBLE_TITLE) {
+          formattedTitle = token.children.length !== 1
+        } else if (state === END_OF_BOX) {
+          onError({
+            lineNumber: token.lineNumber,
+            detail: 'Box title after box content',
+            context: possibleBox.line
+          })
         }
-      })
+      }
+      if (token.type === 'blockquote_close') {
+        if (state === BOX_BODY) {
+          state = END_OF_BOX
+        } else {
+          state = 0
+        }
+      }
+    })
   }
 }
