7d63ac25ce6debcaff24141765901f937a46577c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-25 11:55:13 -0400

    Example customizing the rule

--- tests/javascript.linter.js
+++ tests/javascript.linter.js
@@ -1,8 +1,15 @@
 const standard = require('standard')
 
+const configuration = `
+/* eslint semi: ["error", "always"] */
+/* global assert */
+`
+
+const offset = configuration.split('\n').length - 1
+
 module.exports = async function (basePath, text) {
   return new Promise((resolve, reject) => {
-    standard.lintText(text, {
+    standard.lintText(configuration + text, {
       cwd: basePath,
       fix: false,
       globals: [],
@@ -16,7 +23,7 @@ module.exports = async function (basePath, text) {
         const results = output.results[0].messages
         resolve(results.map(({ ruleId, message, line }) => {
           return {
-            line,
+            line: line - offset,
             message: `${message} (${ruleId})`
           }
         }))
