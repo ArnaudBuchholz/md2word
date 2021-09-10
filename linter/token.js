b0f0f14034f16d168ee6a7cc7950fa2919e7fb93
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-10 17:58:15 -0400

    Token syntax change

--- linter/token.js
+++ linter/token.js
@@ -5,13 +5,13 @@ module.exports = {
   function: (params, onError) => {
     /*
       Allowed tokens (everywhere BUT caption, header)
-        `<<xref:NEXT>>` references the next immediate code or image
-        `<<xref:PREVIOUS>>` references the previous immediate code or image
-        `<<xref:id>>` references the code or image flagged with `id`
+        `{{xref:NEXT}}` references the next immediate code or image
+        `{{xref:PREVIOUS}}` references the previous immediate code or image
+        `{{xref:id}}` references the code or image flagged with `id`
 
       id syntax : [a-z0-9_]+
 
-      only <<xref:id>> in a caption
+      only {{xref:id}} in a caption
     */
   }
 }
