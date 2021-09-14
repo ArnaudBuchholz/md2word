c5bc983c3ad50692faca8200436a2615cf462909
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-14 09:32:55 -0400

    Node 12 support

--- linter/token.js
+++ linter/token.js
@@ -19,7 +19,7 @@ module.exports = {
         inHeading = false
       } else if (token.type === 'blockquote_open') {
         if (++inBlock === 2) {
-          if (xrefs[0]?.possibleBoxHeader) {
+          if (xrefs[0] && xrefs[0].possibleBoxHeader) {
             const xrefToken = xrefs[0].token
             onError({
               lineNumber: xrefToken.lineNumber,
@@ -33,7 +33,7 @@ module.exports = {
         }
       } else if (token.type === 'blockquote_close') {
         if (--inBlock === 0) {
-          if (xrefs[0]?.possibleBoxHeader) {
+          if (xrefs[0] && xrefs[0].possibleBoxHeader) {
             delete xrefs[0].possibleBoxHeader
             xrefs[0].id = true
           }
